53aff51575921631df5b0424ef9d57296cfcfaa9
1ren <2563539699@qq.com>
2019-10-21 14:39:06 +0800

    Lx03

---
+++ Service/Controllers/UsersController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Service.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Json(DAL.UserInfo.Instance.GetCount());
+        }
+        [HttpPut("username")]
+        public ActionResult getUser(string username)
+        {
+            var result = DAL.UserInfo.Instance.GetModel(username);
+        if (result !=null)
+            return Json(Result.Ok(result));
+        else
+        return Json(Result.Err("用户名不存在"));
+        }
+
+
+        // POST api/<controller>
+        [HttpPost]
+        public ActionResult Post([FromBody]Model.UserInfo users)
+        {
+            try
+            {
+                var n = DAL.UserInfo.Instance.Add(users);
+                if (n != 0)
+                return Json(Result.Ok("添加成功"));
+                else
+                    return Json(Result.Ok("用户名不存在"));
+            }
+            catch(Exception ex)
+            {
+                if (ex.Message.ToLower().Contains("primary"))
+                    return Json(Result.Err("用户名已存在"));
+                else if (ex.Message.ToLower().Contains("null"))
+                    return Json(Result.Err("用户名、密码、身份不能为空"));
+                else
+                    return Json(Result.Err(ex.Message));
+            }
+
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
